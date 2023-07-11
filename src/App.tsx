eabf0e96aaccabc459d9745f63ba639376b1e1f5
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-07-10 20:59:36 -0600

    Implement main pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Homepage from "@/pages/Homepage";
+import Pricing from "@/pages/Pricing";
+import Product from "@/pages/Product";
+import NotFound from "@/pages/NotFound";
+
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
